2e2763c33df4c03ed5a82dfe5fc9c06524989d06
Richard <richard.daubner@icloud.com>
2026-07-13 10:50:16 +0200

    adjusted the layout and added full public images folder

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,17 +1,45 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen">
-        <section className="bg-brand p-10">
-            <div>
+        <section className="bg-brand p-10 hidden w-1/2 items-center justify-center lg:flex">
+            <div className="flex max-h-[800px] max-w-[430px] flex-col justify-center space-y-12">
+
+                <Image
+                    src="/assets/icons/logo-full.svg"
+                    alt="logo"
+                    width={224}
+                    height={16}
+                    className="h-auto"
+                />
+
                 <div className="space-y-5 text-white">
                     <h1 className="h1">manage your files the best way</h1>
                     <p className="body-1">this is the place to store your files</p>
                 </div>
+                <Image
+                    src="/assets/images/files.png"
+                    alt="illustration"
+                    width={342}
+                    height={342}
+                    className="transition-all hover:rotate-2 hover:scale-105"
+                />
             </div>
         </section>
-        {children}
+        <section className="flex flex-1 flex-col items-center bg-white p-4 py-10 lg:justify-center lg:p-10 lg:py-0">
+            <div className="mb-16 lg:hidden">
+                <Image
+                    src="/assets/icons/logo-full-brand.svg"
+                    alt="logo"
+                    width={224}
+                    height={82}
+                    className="h-auto w-[200px] lg:w-[250px]"
+                />
+            </div>
+            {children}
+        </section>
     </div>
   )
 }
